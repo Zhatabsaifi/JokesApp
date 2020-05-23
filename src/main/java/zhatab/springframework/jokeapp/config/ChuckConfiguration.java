287c93a5ae42f6b2868dd9bb5def329eda5a722e
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-05-23 23:14:05 +0530

    xml configuration. Closes #2

--- src/main/java/zhatab/springframework/jokeapp/config/ChuckConfiguration.java
+++ src/main/java/zhatab/springframework/jokeapp/config/ChuckConfiguration.java
@@ -1,14 +1,13 @@
 package zhatab.springframework.jokeapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
 }
+// now spring doesn't look it like a configuration.
